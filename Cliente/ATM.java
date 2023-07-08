3a540924d9078556e6e78ea0313a71f6b8647b19
Xrixer <juanjogonzalez003@gmail.com>
2023-07-07 22:50:44 -0400

    Actualizacion de data

--- Cliente/ATM.java
+++ Cliente/ATM.java
@@ -1,4 +1,4 @@
-// package Client;
+1// package Client;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
--- Servidor/Bank.java
+++ Servidor/Bank.java
@@ -1,3 +1,4 @@
+
 // package Server;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
@@ -12,29 +13,28 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 public class Bank {
-	
-	
+
 	static String ultimaCuenta = "";
 	static int iUltimaCuenta = 0;
-	
-	
-	public static void main(String [] args) throws IOException {
-	
+
+	public static void main(String[] args) throws IOException {
+
 		// apertura de archivo cuentas.txt, para revision de integridad y existencia
 
 		try {
 			FileReader fr = new FileReader("Data/Usuarios.txt");
 			BufferedReader br = new BufferedReader(fr);
 			String linea = br.readLine();
-			if(linea == null || linea.length() != 8){ //archivo corrupto
+			if (linea == null || linea.length() != 8) { // archivo corrupto
 				br.close();
 				File archivo = new File("Data/Usuarios.txt");
 				archivo.delete();
 				CrearArchivoUsuarios();
 			}
-			// hace backup de archivo correcto, de existir, por si ocurre algun error, tener respaldo
+			// hace backup de archivo correcto, de existir, por si ocurre algun error, tener
+			// respaldo
 			// HACER
-			
+
 		} catch (IOException e) {
 			// System.out.println("Error al abrir el archivo: " + e.getMessage());
 			CrearArchivoUsuarios();
@@ -50,23 +50,22 @@ public class Bank {
 		iUltimaCuenta = Integer.parseInt(linea);
 		try {
 			System.out.println("El Servidor esta Iniciando....");
-			System.setProperty("java.rmi.server.hostname","127.0.0.1"); 
+			System.setProperty("java.rmi.server.hostname", "127.0.0.1");
 			// esta linea inicia el server
 			LocateRegistry.createRegistry(9100);
 
 			// Get the RMI registry.
 			Registry registry = LocateRegistry.getRegistry("127.0.0.1", 9100);
 
-			
-
 			// Creamos objetos de todos los usuarios desde el archivo C
-			
+
 			UsuarioImpl us;
 			String[] userData;
 			// publicacion en el RMI de la cuenta mas grande usada en texto y en entero
-			us = new UsuarioImpl("", "", "00000000", "", ultimaCuenta,iUltimaCuenta, "",0, "",0);
+			us = new UsuarioImpl("", "", "00000000", "", ultimaCuenta, iUltimaCuenta, "", 0, "", 0);
 			Usuario stub = (Usuario) UnicastRemoteObject.exportObject(us, 0);
-			// se registra bajo el docum "00000000", que es un numero de cedula que nadie puede tener
+			// se registra bajo el docum "00000000", que es un numero de cedula que nadie
+			// puede tener
 			registry.rebind("00000000", stub);
 			// se registran los demas usuarios del archivo
 			while ((linea = br.readLine()) != null) {
@@ -74,8 +73,9 @@ public class Bank {
 				ListaCuentas.add(linea);
 				System.out.println(linea);
 				userData = linea.split("\\|");
-				us = new UsuarioImpl(userData[0], userData[1], userData[2], userData[3], userData[4], Double.parseDouble(userData[5]), 
-											userData[6], Double.parseDouble(userData[7]), userData[8], Double.parseDouble(userData[9]));
+				us = new UsuarioImpl(userData[0], userData[1], userData[2], userData[3], userData[4],
+						Double.parseDouble(userData[5]),
+						userData[6], Double.parseDouble(userData[7]), userData[8], Double.parseDouble(userData[9]));
 				stub = (Usuario) UnicastRemoteObject.exportObject(us, 0);
 				registry.rebind(userData[2], stub);
 			}
@@ -84,11 +84,11 @@ public class Bank {
 			// crea objeto para incluir en el registro los metodos
 			// Instanciamos la clase implementada
 			CrearRMIImplem objRemoto = new CrearRMIImplem();
-			
+
 			// Exportamos el objeto de la clase implementada
-            // Con port=0 se usará el puerto por defecto de RMI, el 1099
+			// Con port=0 se usará el puerto por defecto de RMI, el 1099
 			CrearRMI stubTransacUsuario = (CrearRMI) UnicastRemoteObject.exportObject(objRemoto, 0);
-			
+
 			// Vinculamos el objeto remoto (stub) en el registro (rmiregistry)
 			// Enlazamos el stub y nombramos el objeto remoto RMI como "CrearUsuario"
 			registry.rebind("MetodosServidor", stubTransacUsuario);
@@ -100,8 +100,8 @@ public class Bank {
 			System.out.println("Error del Servidor" + e);
 			System.exit(1);
 		}
-		//br.close();
-		//System.exit(0);
+		// br.close();
+		// System.exit(0);
 	}
 
 	private static void CrearArchivoUsuarios() throws IOException {
@@ -111,5 +111,5 @@ public class Bank {
 		bw.write("00000000");
 		bw.close();
 	}
-	
+
 }
